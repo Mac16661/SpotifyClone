f02a18711868d6504e7836268e800315cc3a0c35
Subhodip Ghosh <macy16661@gmail.com>
2024-07-29 21:01:24 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -3,13 +3,13 @@ import "firebase/firestore";
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyCVVWKyQesRadkVwxtoeuhY5B2ANRokWtU",
-    authDomain: "spotify-ab562.firebaseapp.com",
-    projectId: "spotify-ab562",
-    storageBucket: "spotify-ab562.appspot.com",
-    messagingSenderId: "968280212346",
-    appId: "1:968280212346:web:3c51aecb369098fcb2ccc3",
-    measurementId: "G-5FKW52SLE9"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
   };
 
   firebase.initializeApp(firebaseConfig);
